3c9ed9d02a4d174a82d3821fe1aa9fab4abe6cb5
Kogy <Kogyisaza2004@gmail.com>
2022-08-30 21:21:10 -0500

    Inicio de seccion

---
+++ Back-end/login.php
@@ -0,0 +1,22 @@
+<?php
+
+include("db.php");
+
+$email_I = $_POST['email'];
+$password_I = $_POST['pass'];
+
+$sql1 = "SELECT * FROM `login` WHERE email='$email_I' and pass='$password_I'";
+
+$resultado1 = mysqli_query($db, $sql1);
+$filas = mysqli_num_rows($resultado1);
+
+// while ($row = mysqli_fetch_array($resultado1)) {
+//   $email = $row['email'];
+//   $pass = $row['password'];
+// }
+
+if ($filas) {
+  header("location:../conocenos.html");
+} else {
+  echo "No has ingresado al sistema";
+}
---
+++ Iniciar_seccion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Iniciar sección</title>
+  <link rel="stylesheet" href="Front-end/css/ayudanos.css" />
+  <link rel="stylesheet" href="Front-end/css/bar-lat.css" />
+  <link rel="stylesheet" href="Front-end/css/barra-nav.css">
+  <link rel="stylesheet" href="Front-end/css/general.css">
+
+</head>
+
+<body>
+  <div class="cuerpo">
+    <div class="barra-nav">
+      <nav>
+        <ul>
+          <a href="index.html">
+            <li>Inicio</li>
+          </a>
+          <a href="">
+            <li>Categorías</li>
+          </a>
+          <a href="conocenos.html">
+            <li>Conócenos</li>
+          </a>
+          <div class="buscador"></div>
+        </ul>
+      </nav>
+    </div>
+    <header>
+      <h2>Ayúdanos</h2>
+    </header>
+    <div class="container">
+      <article>
+        Para ayudarnos a ingresar una nueva pagina en la Wiki debes iniciar
+        sesión
+      </article>
+      <div class="articulos">
+        <article>
+          <form action="Back-end/login.php" method="post">
+            <label for="email">Ingresa Correo electrónico</label>
+            <input type="email" name="email" id="email" placeholder="Correo electrónico">
+            <br>
+            <label for="pass">Ingresa la contraseña</label>
+            <input type="password" name="pass" id="pass" placeholder="Contraseña">
+            <br>
+            <button>Ingresar</button>
+          </form>
+          <a href="Registrarse.php">
+            <span>Registrarse</span>
+          </a>
+        </article>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
